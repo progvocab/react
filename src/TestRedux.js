2465e9e4bea8751cd5f7d8f125ac483db33bfb3b
progvocab <progvocab@gmail.com>
2021-06-25 22:27:27 +0530

    1 file modified

--- src/TestRedux.js
+++ src/TestRedux.js
@@ -4,12 +4,12 @@ import './style.css';
 export default function TestRedux() {
   // Define an initial state value for the app
   const initialState = {
-    value: 0
+    value: ':)'
   };
 
   return (
     <div>
-      <h1>Bye StackBlitz! {initialState}</h1>
+      <h1>Bye StackBlitz! {initialState.value}</h1>
       <p>Start editing to see some magic happen :)</p>
     </div>
   );
